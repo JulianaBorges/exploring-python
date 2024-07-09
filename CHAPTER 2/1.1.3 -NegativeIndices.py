5a076854645597474bdb80b806628cd30b308dce
Juliana Borges <Juuh.borges@hotmail.com>
2024-07-09 19:19:58 -0300

    Negative Indices

---
+++ CHAPTER 2/1.1.3 -NegativeIndices.py
@@ -0,0 +1,11 @@
+    #Negative Indice, lê o final da lista 
+    #bin_Fruit[-apresenta : -remove]
+
+bin_Fruit = ['Watermelon','Strawberry','Passion Fruit','Orange','Pineapple', 'Apple','Prunes','Blackberry']
+
+print(bin_Fruit[:-1]) #remove o ultimo elemento
+print(bin_Fruit[-1:]) #apresenta o ultimo elemento
+print(bin_Fruit[-3:]) 
+print(bin_Fruit[:-3])
+print(bin_Fruit[-2:-1]) #apresenta o penultimo e remove o ultimo
+print(bin_Fruit[-4:-3])
