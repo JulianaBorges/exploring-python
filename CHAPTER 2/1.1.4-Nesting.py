9d44d0c7da794ea95c1dcbae40bca7b5e4455a46
Juliana Borges <Juuh.borges@hotmail.com>
2024-07-09 19:21:45 -0300

    Nesting

---
+++ CHAPTER 2/1.1.4-Nesting.py
@@ -0,0 +1,9 @@
+    #busca posiçôes na sublistas(lista de lista).
+    
+a = [1,2,[100,200,300,25,12,38,60,2,6,17,45],6,['frutas','Animais'],7, [True,False]]
+print(max(a[2]))
+print(min(a[2]))
+print(a[3])
+print(a[2][1])
+print(a[4][0])
+print(a[-1][1]) #joking whith slicing and negative
