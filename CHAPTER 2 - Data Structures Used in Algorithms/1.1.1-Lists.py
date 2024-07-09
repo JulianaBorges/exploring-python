438042bbd24dd05d8435dbaa4e836d778b3d4683
Juliana Borges <Juuh.borges@hotmail.com>
2024-07-09 19:11:51 -0300

    CHAPTER 2 - Data Structures Used in Algorithms

---
+++ CHAPTER 2 - Data Structures Used in Algorithms/1.1.1-Lists.py
@@ -0,0 +1,12 @@
+#cria a lista.
+lista_a = ["John", 33, "Toronto", True]
+
+#apresenta a lista
+print(lista_a)
+
+#apresenta o tipo da lista.
+type(lista_a)
+
+#busca o dado na lista da posição selecionada.
+bin_colors = ['red','green','blue','yellow']
+print(bin_colors[1])
