2761e8b4e09baec519608a86c12a0447e26f670f
Juliana Borges <Juuh.borges@hotmail.com>
2024-07-11 00:43:44 -0300

    Exercicios: soma-dois, soma-Juros,soma-raio-circular

--- CHAPTER 2/1.1.2-Slicing.py
+++
@@ -1,6 +0,0 @@
- #buscando dados das lista com fatiamentos
-bin_colors = ['red','green','blue','yellow']
-print(bin_colors[1])
-print(bin_colors[0:3])
-print(bin_colors[2:])
-print(bin_colors[:2])
---
+++ Exercicios python/soma-raio-circulo.py
@@ -0,0 +1,8 @@
+import math
+
+#entrada da area do ciculo.
+Raio = float(input('Qual é o raio do Circulo?' ))
+
+Area = math.pi * (Raio ** 2)
+
+print(f'{Area:.2f}')
